f96c2ddab7d1bb29b6b25b57f064cd7b4694501b
Gustavs Venters <gustavs.venters@gmail.com>
2017-07-01 19:26:25 +0200

    Initial commit

---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,91 @@
+import org.apache.commons.io.FileUtils
+import org.apache.commons.lang3.SystemUtils
+import java.io.*
+import java.net.URL
+
+
+fun main(args: Array<String>) {
+    val fileURLs = readLinesFromFile("links.txt")
+    for (fileURL in fileURLs) {
+        val fileName = fileURL.substringBefore(".zip").split("/").last()
+        val projectName = fileName.split("-").first()
+        val projectVersion = fileName.split("-")[1]
+        val projectKey = "QC:$projectName"
+        val projectFullName = "QC - $projectName".replace("_", " ")
+
+        val zipFile = File("zipfile.zip")
+        val oldFiles = File(".").listFiles()
+        val url = URL("http://java.labsoft.dcc.ufmg.br/qualitas.class/corpus/$fileName")
+        FileUtils.copyURLToFile(url, zipFile)
+
+        // unzip
+        val pb = ProcessBuilder("jar", "xf", "zipfile.zip")
+        val p = pb.start()
+        val returnCode = p.waitFor()
+        val reader = BufferedReader(InputStreamReader(p.inputStream))
+        val allText = reader.use(BufferedReader::readText)
+        print(allText)
+        if (returnCode != 0)
+            throw Throwable("unzip error")
+
+        // get unzipped file name
+        val newFiles = File(".").listFiles()
+        for (document in newFiles) {
+            if (!oldFiles.contains(document)) {
+                // make sonar-scanner.properties
+                val srcFolder = document
+                File(srcFolder.canonicalPath + File.separator + "sonar-scanner.properties").printWriter().use { pw ->
+                    pw.println("sonar.projectKey=$projectKey")
+                    pw.println("sonar.projectName=$projectFullName")
+                    pw.println("sonar.projectVersion=$projectVersion")
+                    pw.println("sonar.sources=.")
+                    pw.println("sonar.language=java")
+                    pw.println("sonar.binaries=.")
+                    pw.println("sonar.java.binaries=.")
+                    pw.println("sonar.host.url=http://sonar.inf.unibz.it")
+                    //pw.println("sonar.host.url=http://localhost:9000")
+
+                }
+
+                //perform scan
+                val scannerCmd =
+                        if (SystemUtils.IS_OS_WINDOWS)
+                        "sonar-scanner.bat"
+                    else
+                        "sonar-scanner"
+                val pbScan = ProcessBuilder(scannerCmd, "-Dproject.settings=sonar-scanner.properties")
+                        .directory(File(srcFolder.canonicalPath + File.separator))
+
+                val pScan = pbScan.start()
+                val readerScan = BufferedReader(InputStreamReader(pScan.inputStream))
+                val allTextScan = readerScan.use(BufferedReader::readText)
+                print(allTextScan)
+                val stderr = BufferedReader(InputStreamReader(pScan.errorStream))
+                val errText = stderr.use(BufferedReader::readText)
+                print(errText)
+
+                val returnCodeScan = pScan.waitFor()
+                if (returnCodeScan != 0)
+                    throw Throwable("scan error")
+
+                //delete folder
+                document.deleteRecursively()
+                break
+            }
+        }
+        // delete downloaded zip
+        zipFile.deleteRecursively()
+    }
+}
+
+fun readLinesFromFile(file: String): List<String> {
+    val result = mutableListOf<String>()
+    BufferedReader(FileReader(file)).use { br ->
+        do {
+            val line = br.readLine()
+            if (line != null)
+                result.add(line)
+        } while (line != null)
+    }
+    return result
+}
